61e523bcdbbfe0e0e79a6029b89c9ec0a2bcf5ab
Andrii Mishchenko <vinnypuh1818@gmail.com>
2023-07-01 23:54:17 +0300

    logic tip calc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  const [price, setPrice] = useState();
+  const [tip, setTip] = useState();
+  const [person, setPerson] = useState(1);
+
+  const [tipAmount, setTipAmount] = useState();
+  const [total, setTotal] = useState()
+  const [totalTipAmount, setTotalTipAmount] = useState('sdffsd')
+
+  useEffect(() => {
+    if(person < 1){
+      setTotal(price)
+    }if(person >= 1){
+      const calcTotal = (price + tipAmount) / person;
+      setTotal(calcTotal.toFixed(2))
+    }
+  })
+
+  useEffect(() => {
+    if(person === 1){
+      setTotalTipAmount(tipAmount)
+    }if(person > 1){
+      const calcTip = tipAmount / person
+      setTotalTipAmount(calcTip)
+    }
+  })
+
+  useEffect(() => {
+    const calcTip = price * (tip / 100)
+    setTipAmount(calcTip)
+  })
+
+  const getTip = (e) => {
+    e.preventDefault();
+    setTip(Number(e.target.textContent))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input
+          value={price}
+          onChange={(e) => setPrice(Number(e.target.value))}
+        ></input>
+      <div>
+        <button onClick={getTip}>5</button>
+        <button onClick={getTip}>10</button>
+        <button onClick={getTip}>15</button>
+        <button onClick={getTip}>20</button>
+        <button onClick={getTip}>25</button>
+      </div>
+      <div>Person
+        <input
+          value={person}
+          onChange={(e) => setPerson(Number(e.target.value))}
+        ></input>
+      </div>
+      </form>
+
+      <p>price: {price}</p>
+      <p>tip: {tip}</p>
+      <p>person: {person}</p>
+
+      <div>tipAmount: {tipAmount}</div>
+      <div>Total Amount for person: {total}</div>
+      <div>Tip Amount for person: {totalTipAmount}</div>
+    </>
   );
 }
 
